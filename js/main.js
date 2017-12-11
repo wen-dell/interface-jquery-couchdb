302ef461f619e74ee5feac9cc55536469ca95c6c
Wendell <wendellmccaulay@hotmail.com>
2017-12-10 21:12:51 -0300

    Criação do método para atualizar

--- js/main.js
+++ js/main.js
@@ -40,3 +40,26 @@ function apagar() {
         }
     });
 }
+
+function atualizar() {
+
+    let nome = $("#nome").val();
+    let profissao = $("#profissao").val();
+    let estado = $("#estado").val();
+
+    let objeto = {
+        nome: nome,
+        profissao: profissao,
+        estado: estado,
+        _rev: "6-5a133f0846bdeb0e4c837409d5354bad"
+    };
+
+    $.ajax({
+        url: "http://localhost:5984/meu_bd/001/",
+        type: "put",
+        data: JSON.stringify(objeto),
+        success: function (result) {
+            console.log(result);
+        }
+    });
+}
